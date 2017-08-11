b40eaa4e247b918b2ffcb7b33be2d43e525345de
张磊 <zhanglei@epb360.com>
2017-08-11 14:16:38 +0800

    修改story配置

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,4 @@
 import { configure } from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
-
-setOptions({
-    name: 'react-components',
-    goFullScreen: false,
-    showLeftPanel: true,
-    showDownPanel: false,
-    showSearchBox: false,
-    downPanelInRight: false
-});
 
 function loadStories() {
   require('../stories') ;
