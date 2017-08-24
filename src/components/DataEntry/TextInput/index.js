323bd70de0c6c8edf1df79064b15b1cf78f66b0f
张磊 <zhanglei@epb360.com>
2017-08-24 18:10:22 +0800

    新增textinput

--- src/components/DataEntry/TextInput/index.js
+++ src/components/DataEntry/TextInput/index.js
@@ -1,14 +1,50 @@
 import React ,{ Component } from 'react';
 
+import { StyleSheet } from '../../../../src';
+import createDOMElement from '../../../../modules/createDOMElement';
+
 
 class TextInput extends Component {
     static displayName = 'TextInput';
 
-    render(){
-        return(
-            <div>TextInput</div>
-        )
+    static defaultProps = {
+        multiline:false,
+        onChange:()=>{},
+        onChangeText:()=>{},
+        pattern: undefined
     }
+
+    _customEvent = {
+        onChange:true,
+        onChangeText:true,
+    }
+
+    render(){
+        console.log('textinput--------------->',this.porps);
+        const {
+            multiline,
+        } = this.props;
+
+        const component = multiline === true ? 'textarea' :'input';
+        const resolveProps = this._resolveProps(this.props);
+
+        return createDOMElement(component, this.resolveProps);
+    }
+
+    //1.处理input事件
+    _resolveProps = (props)=> {
+        Object.keys(props).forEach(item=>{
+            if(this._customEvent[item]){
+                delete props.item
+            }
+        });
+        console.log('处理后的inputprops-------------->',props);
+
+        return props;
+    }
+
+
+
 }
 
 module.exports = TextInput;
--- stories/components/DataEntry/TextInput/index.js
+++ stories/components/DataEntry/TextInput/index.js
@@ -3,9 +3,18 @@ import React from 'react';
 import { TextInput } from '../../../../src';
 
 const TextInputScreen = ()=> {
+
+    const _onChangeText = rs=> {
+        console.log('rs----------------->',rs);
+    }
+
     return(
-        <TextInput />
+        <TextInput 
+            onChangeText={ _onChangeText }
+        />
     )
 }
 
 module.exports = TextInputScreen;
+
+
